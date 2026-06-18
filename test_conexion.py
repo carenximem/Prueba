938b6641f8ebbd633c6e2d45bd5a697eaa485a4c
carenximem <119344801+carenximem@users.noreply.github.com>
2026-06-18 14:14:38 -0500

    Cambios FINALES
    Cambios FINALES

--- test_conexion.py
+++ test_conexion.py
@@ -2,6 +2,6 @@ from app.database import engine
 
 try:
     with engine.connect() as conn:
-        print("✅ Conexión exitosa a SQL Server")
+        print(" Conexión exitosa a SQL Server")
 except Exception as e:
-    print(f"❌ Error de conexión: {e}")
+    print(f" Error de conexión: {e}")
